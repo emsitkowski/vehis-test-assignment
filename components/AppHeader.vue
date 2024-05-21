5f02e4d88bda6f324ccd38f803a7817c53e7b8bc
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-21 21:44:20 +0200

    style: add margin bottom to app header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center items-center flex-wrap gap-12">
+  <div class="flex justify-center items-center flex-wrap gap-12 mb-16">
     <AppLogo />
     <h1 class="text-4xl font-semibold text-center">{{ text }}</h1>
   </div>
