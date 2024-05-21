7cfb33611ed6c21de7551c699fbc28eebb4cfe93
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-21 12:04:47 +0200

    feat: create app header component

---
+++ components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-center items-center flex-wrap gap-12">
+    <AppLogo />
+    <h1 class="text-4xl font-semibold text-center">{{ text }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
