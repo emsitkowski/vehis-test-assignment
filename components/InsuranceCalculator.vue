b8174442745c3e14cdcb71f6c6292268f3e07492
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-21 21:48:50 +0200

    feat: create insurance calculator component

---
+++ components/InsuranceCalculator.vue
@@ -0,0 +1,17 @@
+<template>
+  <Card class="max-w-lg mx-auto">
+    <CardHeader>
+      <CardTitle> Dane samochodu</CardTitle>
+      <CardDescription>
+        Wypełnij dane pojazdu, aby obliczyć roczną składkę ubezpieczenia OC/AC, z możliwością wyliczenia kwoty raty.
+      </CardDescription>
+    </CardHeader>
+    <CardContent> content goes here </CardContent>
+  </Card>
+</template>
+
+<script setup lang="ts">
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+</script>
+
+<style scoped></style>
