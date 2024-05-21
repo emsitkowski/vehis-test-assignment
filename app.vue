2fe538f7ffa6deb09afea85b1ae423cdb2493271
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-21 21:50:46 +0200

    feat: add insurance calculator component

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container mx-auto pt-20">
+    <!-- Header -->
+    <AppHeader text="Kalkulator AC/OC" />
+
+    <!-- Calculator -->
+    <InsuranceCalculator />
   </div>
 </template>
--- components/InsuranceCalculator.vue
+++ components/InsuranceCalculator.vue
@@ -1,12 +1,18 @@
 <template>
   <Card class="max-w-lg mx-auto">
     <CardHeader>
+      <!-- Card title -->
       <CardTitle> Dane samochodu</CardTitle>
+
+      <!-- Card description -->
       <CardDescription>
         Wypełnij dane pojazdu, aby obliczyć roczną składkę ubezpieczenia OC/AC, z możliwością wyliczenia kwoty raty.
       </CardDescription>
     </CardHeader>
-    <CardContent> content goes here </CardContent>
+    <CardContent>
+      <!-- Insurance calculator -->
+      <InsuranceCalculatorForm />
+    </CardContent>
   </Card>
 </template>
 
