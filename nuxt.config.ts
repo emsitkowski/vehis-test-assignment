b31b004e4c22f2aa6d751e3a4c5361cd935bdcea
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-22 20:28:38 +0200

    feat: disable zooming while filling form fields on mobile devices

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      meta: [{ name: "viewport", content: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" }],
+    },
+  },
   css: ["~/assets/scss/main.scss"],
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@pinia/nuxt"],
   shadcn: {
