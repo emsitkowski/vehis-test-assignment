23eb773b3660103463de0571b66ee26527f6ad34
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-22 10:51:19 +0200

    fix: app header text

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto pt-20">
     <!-- Header -->
-    <AppHeader text="Kalkulator AC/OC" />
+    <AppHeader text="Kalkulator OC/AC" />
 
     <!-- Calculator -->
     <InsuranceCalculator />
